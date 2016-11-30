662696c36352cccb18515aa58de8cb63b7575224
gabycommander <gabriel.selegean@yahoo.ro>
2016-11-30 10:57:18 +0200

    commit

---
+++ compute2numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1, num2;
+	cout<<"This program will compute 2 integer numbers"<<endl;
+	cout<<"number 1 : "; 
+	cin>>num1;
+	cout<<"number 2 : "; 
+	cin>>num2;
+	cout<<num1<<" + "<<num2<<" = "<<num1 + num2<<endl;
+	
+	return 0;
+}
